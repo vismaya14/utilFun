56c9fbe2ae2725d4448413b5c7a10865b7f39ae8
Vismaya Mohan <vismaya.mohan@flexiloans.com>
2018-11-29 12:10:13 +0530

    minor updates

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const jwt = require("./JwtMiddleware");
 const awsDNSLookup = require("./DNSLookup");
-const jwt = require('./middleware/jwtMiddleware');
 
 module.exports = {
     jwt: jwt.jwtMiddleware,
